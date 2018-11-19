0bcd7c111b21e66c42d605cd4c266cca9e722ca6
Aymeric Neveu <emploi.neveu@gmail.com>
2018-11-19 11:53:07 +0100

    exo6

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $int = 20;
+    while ($int >0) {
+      echo 'C\'est presque bon' . '<br>';
+      $int--;
+    }
+    ?>
+  </body>
